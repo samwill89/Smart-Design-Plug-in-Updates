b93a5ec7d48984a1288ba513145b5566317796ce
Omar Arafa <omararafa43@gmail.com>
2020-08-02 05:00:45 +0200

    adding the connector WPF

--- Smart Design Plug-in Updates/SynchronizeFileMaker.cs
+++ Smart Design Plug-in Updates/SynchronizeFileMaker.cs
@@ -3,9 +3,11 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp1;
 
 namespace Smart_Design_Plug_in_Updates
 {
@@ -18,6 +20,16 @@ namespace Smart_Design_Plug_in_Updates
           ElementSet elements)
         {
 
+            #region Intialize window
+            MainWindow x = new MainWindow();
+            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            double windowWidth = x.Width;
+            double windowHeight = x.Height;
+            x.Left = (screenWidth / 2) - (windowWidth / 2);
+            x.Top = (screenHeight / 2) - (windowHeight / 2);
+            x.ShowDialog();
+            #endregion
 
 
             return Result.Succeeded;
